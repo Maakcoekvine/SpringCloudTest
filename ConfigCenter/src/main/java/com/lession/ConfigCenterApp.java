6c10328c6c53e4a0063ec679231fb2c6968911fa
=Phelix <=phelixmaak@gmail.com>
2025-10-01 13:50:11 +0800

    spring cloud config center

---
+++ ConfigCenter/src/main/java/com/lession/ConfigCenterApp.java
@@ -0,0 +1,16 @@
+package com.lession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigCenterApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterApp.class, args);
+    }
+}
